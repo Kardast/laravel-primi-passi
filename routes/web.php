b9aacd9b481839cc2d5a017ddaecfedf104f1a9f
Kardast <dottorsf@gmail.com>
2022-07-11 17:24:09 +0200

    Views and links

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/duck', function () {
+    return view('duck');
 });
